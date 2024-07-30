6ea43f4ce8875230c48069de342ffe726402c491
lihon <gelf.learing@gmail.com>
2024-07-30 15:38:57 +0300

    updating my work

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button className="btn btn-primary">Primary</button>
+    </div>
+  )
+}
