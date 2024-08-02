ead85ca730dad206d13a34dc1f6af4277a49130d
lihon <gelf.learing@gmail.com>
2024-08-02 23:08:42 +0300

    gemini api worked

--- components/message.jsx
+++ components/message.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { IoIosPerson } from "react-icons/io";
+import { SiGooglegemini } from "react-icons/si";
 
 const Message = ({ role, content }) => {
   return (
     <div className='grid grid-cols-[25px_1fr] gap-5 p-5'>
-        <div>{role}"-"</div>
+        <div>
+          {role === 'user' ? <IoIosPerson /> : <SiGooglegemini />}
+        </div>
         <div>{content}</div>
     </div>
   )
